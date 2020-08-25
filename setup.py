1fc624f6eeed0d1f326a136e7ed7189d5c6d4661
Cézar Lemos <cezarcbl@protonmail.com>
2020-08-25 11:56:05 -0300

    add licence

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='bboxes',
-    version='0.1dev',
+    license='MIT',
+    author='Cézar Lemos',
+    author_email='cezarcbl@pm.me',
+    version='0.0.ssggcnn1',
     packages=['bboxes',],
     install_requires=['numpy', 'matplotlib', 'opencv-python'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='Some utils to work with bounding boxes',
     long_description=open('README.md').read(),
 )
