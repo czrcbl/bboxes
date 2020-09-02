c73e80f1e4e5c95719896e2428649160c188134a
Cézar Lemos <cezarcbl@protonmail.com>
2020-09-02 19:30:58 -0300

    update tests

--- tests/test_bboxes.py
+++ tests/test_bboxes.py
@@ -1,8 +1,5 @@
 import os
 import sys
-# path = os.path.dirname(os.path.dirname(os.path.abspath(os.path.realpath(__file__))))
-# if path not in sys.path:
-#     sys.path.insert(0, path)
 import unittest
 import numpy as np
 from bboxes import BboxList, Bbox
